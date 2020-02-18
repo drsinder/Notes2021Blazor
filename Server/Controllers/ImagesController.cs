1fbc16d8ccf97ff23886b25b8da6a0d01df6c8e2
Dale Sinder <sinder@illinois.edu>
2020-02-17 18:05:30 -0600

    Add foundation for uploading images

---
+++ Server/Controllers/ImagesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.IO;
+using Microsoft.AspNetCore.Http.Features;
+using Notes2021Blazor.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Notes2021Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [Route("api/[controller]/{file}")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private NotesDbContext _db;
+
+        public ImagesController(NotesDbContext env)
+        {
+            this._db = env;
+        }
+
+        [HttpGet]
+        public async Task<FileResult> Get(string filename)
+        {
+            SQLFile myFile = await _db.SQLFile.SingleAsync(p => p.FileName == filename);
+
+            return File((await (_db.SQLFileContent.SingleAsync(m => m.SQLFileId == myFile.FileId))).Content,
+                System.Net.Mime.MediaTypeNames.Application.Octet,
+                myFile.FileName);
+        }
+
+
+        [HttpPost("[action]")]
+        public async Task Save(IList<IFormFile> UploadFiles)
+        {
+            //long size = 0;
+            try
+            {
+                foreach (IFormFile file in UploadFiles)
+                {
+                    var filename = ContentDispositionHeaderValue
+                            .Parse(file.ContentDisposition)
+                            .FileName
+                            .Trim('"');
+
+                    string nameonly = Path.GetFileNameWithoutExtension(filename);
+                    string extonly = Path.GetExtension(filename);
+
+                    SQLFile SqlFile = new SQLFile();
+                    SQLFileContent SqlFileContent = new SQLFileContent { Content = new byte[file.Length] };
+                    using (Stream reader = file.OpenReadStream())
+                    {
+                        int x = await reader.ReadAsync(SqlFileContent.Content, 0, (int)file.Length);
+                        if (x != file.Length)
+                        {
+                            Response.Clear();
+                            Response.StatusCode = 204;
+                            Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File failed to upload";
+                            return;
+                        }
+                    }
+
+                    SqlFile.ContentType = file.ContentType;
+                    SqlFile.Contributor = "unknown";
+                    SqlFile.FileName = filename;
+
+                    _db.SQLFile.Add(SqlFile);
+                    await _db.SaveChangesAsync();
+                    long retId = SqlFile.FileId;
+
+                    SqlFileContent.SQLFileId = retId;
+                    _db.SQLFileContent.Add(SqlFileContent);
+                    await _db.SaveChangesAsync();
+                    retId = SqlFileContent.SQLFileId;
+
+                    string fname = nameonly + extonly;
+                    SqlFile.FileName = fname;
+                    _db.Entry(SqlFile).State = EntityState.Modified;
+                    await _db.SaveChangesAsync();
+
+
+                    //filename = hostingEnv.ContentRootPath + $@"\{filename}";
+                    //size += (int)file.Length;
+                    //if (!System.IO.File.Exists(filename))
+                    //{
+                    //    using (FileStream fs = System.IO.File.Create(filename))
+                    //    {
+                    //        file.CopyTo(fs);
+                    //        fs.Flush();
+                    //    }
+                    //}
+                }
+            }
+            catch (Exception e)
+            {
+                Response.Clear();
+                Response.StatusCode = 204;
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File failed to upload";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+            }
+        }
+        [HttpPost("[action]")]
+        public void Remove(IList<IFormFile> UploadFiles)
+        {
+            //try
+            //{
+            //    var filename = hostingEnv.ContentRootPath + $@"\{UploadFiles[0].FileName}";
+            //    if (System.IO.File.Exists(filename))
+            //    {
+            //        System.IO.File.Delete(filename);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Response.Clear();
+            //    Response.StatusCode = 200;
+            //    Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "File removed successfully";
+            //    Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+            //}
+        }
+    }
+}
