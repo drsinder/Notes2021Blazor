a47848169686c17ab838984004f6b7af5886719d
Dale Sinder <sinder@illinois.edu>
2020-02-19 08:43:30 -0600

    Copyright License additions

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,29 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿/*--------------------------------------------------------------------------
+    **
+    ** Copyright(c) 2020, Dale Sinder
+    **
+    ** Name: Program.cs
+    **
+    ** Description:
+    **      Program main enrtyp point - startup
+    **
+    ** This program is free software: you can redistribute it and/or modify
+    ** it under the terms of the GNU General Public License version 3 as
+    ** published by the Free Software Foundation.   
+    **
+    ** This program is distributed in the hope that it will be useful,
+    ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    ** GNU General Public License version 3 for more details.
+    **
+    **  You should have received a copy of the GNU General Public License
+    **  version 3 along with this program in file "license-gpl-3.0.txt".
+    **  If not, see<http: //www.gnu.org/licenses/gpl-3.0.txt>.
+    **
+    **--------------------------------------------------------------------------*/
+
+
+using Microsoft.AspNetCore.Blazor.Hosting;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
 using Blazored.Modal;
