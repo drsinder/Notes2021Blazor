e24de8eaf4fcf9849569424481a7108ab6904c45
Dale Sinder <sinder@illinois.edu>
2020-02-21 18:31:14 -0600

    Make archive choice friendlier

---
+++ Server/Controllers/ArchiveController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes2021Blazor.Shared;
+
+namespace Notes2021Blazor.Server.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/[controller]")]
+    [Route("api/[controller]/{sid}")]
+
+    [ApiController]
+    public class ArchiveController : ControllerBase
+    {
+        private readonly NotesDbContext _db;
+
+        public ArchiveController(NotesDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<List<NoteHeader>> Get(string sid)
+        {
+            int fileid = int.Parse(sid);
+            List<NoteHeader> list = new List<NoteHeader>();
+
+            NoteFile noteFile = _db.NoteFile.SingleOrDefault(p => p.Id == fileid);
+
+            for ( int i = 0; i <= noteFile.NumberArchives; i++)
+            {
+                NoteHeader header = _db.NoteHeader.Where(p => p.ArchiveId == i).OrderBy(p => p.CreateDate).FirstOrDefault();
+                list.Add(header);
+            }
+        
+            return list;
+        }
+
+    }
+}
