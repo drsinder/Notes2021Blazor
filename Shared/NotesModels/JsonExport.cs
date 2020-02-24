ad0b7f398afc1f21e2e4464f57e17bedbeb0a05a
Dale Sinder <sinder@illinois.edu>
2020-02-23 22:16:37 -0600

    Export of json files for a notefile.  Will permit easy and exact import.

---
+++ Shared/NotesModels/JsonExport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes2021Blazor.Shared
+{
+    public class JsonExport
+    {
+        public List<NoteHeader> NoteHeaders { get; set; }
+        public List<NoteContent> NoteContents { get; set; }
+        public List<Tags> Tags { get; set; }
+    }
+}
